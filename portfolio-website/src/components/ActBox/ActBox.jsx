289f6d8f7cb31c30232013607101393b2ae9fe26
Rahul Soni <rahulgan828@gmail.com>
2024-03-31 12:30:30 +1100

    Responsive changes

--- portfolio-website/src/components/ActBox/ActBox.jsx
+++ portfolio-website/src/components/ActBox/ActBox.jsx
@@ -6,12 +6,12 @@ const ActBox = () => {
         <div className="Act-box">
             <div className="Act-Header">Activities</div>
             <div className="Act-1 Activities">
-                <div className="Act-Logo">• UNSW Innovator Pro</div>
-                <div>Aug - Sep 2022</div>
-            </div>
-            <div className="Act-2 Activities">
                 <div className="Act-Logo">• Media Director</div>
                 <div>Feb - Dec 2023</div>
+            </div>
+            <div className="Act-2 Activities">
+                <div className="Act-Logo">• UNSW Innovator Pro</div>
+                <div>Aug - Sep 2022</div>
             </div>
             <div className="Act-3 Activities">
                 <div className="Act-Logo">• Python Bootcamp</div>
--- portfolio-website/src/components/ExpBox/ExpBox.jsx
+++ portfolio-website/src/components/ExpBox/ExpBox.jsx
@@ -10,12 +10,12 @@ const ExpBox = () => {
         <div className="Exp-box">
             <div className="Exp-Header">Experience</div>
             <div className="Exp-1 Experience">
-                <div className="Exp-Logo">• <img src={Funfox} alt="logo" /></div>
-                <div>May - Sep 2023</div>
-            </div>
-            <div className="Exp-2 Experience">
                 <div className="Exp-Logo">• <img src={Datacom} alt="logo" /></div>
                 <div>Sep - Dec 2023</div>
+            </div>
+            <div className="Exp-2 Experience">
+                <div className="Exp-Logo">• <img src={Funfox} alt="logo" /></div>
+                <div>May - Sep 2023</div>
             </div>
             <div className="Exp-3 Experience">
                 <div className="Exp-Logo">• <img src={Indsoc} alt="logo" /></div>
